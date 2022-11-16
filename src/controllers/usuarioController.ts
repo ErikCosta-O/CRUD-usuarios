4435180bf423e6651dc6284691429e27030e043a
ERIK COSTA OLIVEIRA <erik.coliveira4@senacsp.edu.br>
2022-11-16 11:47:07 -0300

    read, create e update

--- src/controllers/usuarioController.ts
+++ src/controllers/usuarioController.ts
@@ -9,3 +9,41 @@ export const index = async (req:Request, res:Response)=>{
         users
     })
 };
+export const cadastrar = async (req:Request, res:Response)=>{
+    res.render("pages/cadastrar",{
+    })
+};
+export const cadastroUsuario = async(req:Request,res:Response) =>{
+    const {nome, email} =req.body
+    if(nome && email){
+        await Usuario.create({
+            nome,
+            email
+        })
+    }
+    res.redirect('/usuarios')
+};
+
+export const editaUsuario = async(req:Request,res:Response)=>{
+    const{id} =req.params
+    const user =await Usuario.findByPk(id)
+    res.render('pages/editar',{
+        user,
+        id
+    })
+};
+
+export const atualizaUsuario = async(req:Request,res:Response)=>{
+    const {id} = req.params
+    const {nome, email} = req.body
+
+    await Usuario.update({
+        nome,
+        email
+    },{
+        where:{
+            id:id
+        }
+    })
+    res.redirect('/usuarios')
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,5 +7,8 @@ router.get('/',(req:Request,res:Response)=>{
     res.send("!!! -TESTE-!!!")
 });
 router.get('/usuarios',usuarioController.index);
+router.get('/cadastrar',usuarioController.cadastrar);
+router.post('/cadastro', usuarioController.cadastroUsuario);
+router.get('/editar/:id', usuarioController.editaUsuario);
 
 export default router;
